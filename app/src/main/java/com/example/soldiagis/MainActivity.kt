117253edb4d04166e2c2d142f5f6371f4ab77558
soldia <jiseong.r@gmail.com>
2024-02-21 10:07:23 +0900

    push alarm complete
    alarm click app run: ing

--- app/src/main/java/com/example/soldiagis/MainActivity.kt
+++ app/src/main/java/com/example/soldiagis/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.soldiagis
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.drawable.Icon
+import android.media.RingtoneManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -21,6 +27,11 @@ import java.io.IOException
 import java.nio.charset.Charset
 import com.naver.maps.map.overlay.Align
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.overlay.OverlayImage.fromResource
 import com.naver.maps.map.util.FusedLocationSource
@@ -46,6 +57,16 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+            Log.d("token", token)
+        })
 
         // 뷰 역할을 하는 프래그먼트 객체 얻기
         val fm = supportFragmentManager
@@ -307,5 +328,74 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         runOnUiThread {
             Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
         }
+    }
+}
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    private val TAG = "FCM Log"
+
+    override fun onNewToken(token: String) {
+        //새로운 token이 생성될때마다 호출되는 callback
+        Log.d("onNewToken","${token}")
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: " + remoteMessage!!.from)
+
+        // Notification 메시지를 수신할 경우는
+        // remoteMessage.notification?.body!! 여기에 내용이 저장되어있다.
+        // Log.d(TAG, "Notification Message Body: " + remoteMessage.notification?.body!!)
+
+        if(remoteMessage.data.isNotEmpty()){
+            Log.i("바디: ", remoteMessage.data["body"].toString())
+            Log.i("타이틀: ", remoteMessage.data["title"].toString())
+            sendNotification(remoteMessage)
+        }
+
+        else {
+            Log.i("수신에러: ", "data가 비어있습니다. 메시지를 수신하지 못했습니다.")
+            Log.i("data값: ", remoteMessage.data.toString())
+        }
+    }
+
+    private fun sendNotification(remoteMessage: RemoteMessage) {
+        // RequestCode, Id를 고유값으로 지정하여 알림이 개별 표시되도록 함
+        val uniId: Int = (System.currentTimeMillis() / 7).toInt()
+
+        // 일회용 PendingIntent
+        // PendingIntent : Intent 의 실행 권한을 외부의 어플리케이션에게 위임한다.
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) // Activity Stack 을 경로만 남긴다. A-B-C-D-B => A-B
+        val pendingIntent = PendingIntent.getActivity(this, uniId, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        // 알림 채널 이름
+//        val channelId = getString(R.string.firebase_notification_channel_id)
+        val channelId = "11"
+
+        // 알림 소리
+        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        // 알림에 대한 UI 정보와 작업을 지정한다.
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher) // 아이콘 설정
+            .setContentTitle(remoteMessage.data["body"].toString()) // 제목
+            .setContentText(remoteMessage.data["title"].toString()) // 메시지 내용
+            .setAutoCancel(true)
+            .setSound(soundUri) // 알림 소리
+            .setContentIntent(pendingIntent) // 알림 실행 시 Intent
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // 오레오 버전 이후에는 채널이 필요하다.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, "Notice", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        // 알림 생성
+        notificationManager.notify(uniId, notificationBuilder.build())
     }
 }
