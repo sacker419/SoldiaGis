fe7c2b7b6549f298de440476b6bea873b774ec1f
soldia <jiseong.r@gmail.com>
2024-02-23 14:57:03 +0900

    text correction

--- app/src/main/java/com/example/soldiagis/MainActivity.kt
+++ app/src/main/java/com/example/soldiagis/MainActivity.kt
@@ -60,12 +60,13 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
             if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
                 return@OnCompleteListener
             }
 
             // Get new FCM registration token
             val token = task.result
-            Log.d("token", token)
+            Log.d("FCM token", token)
         })
 
         // 뷰 역할을 하는 프래그먼트 객체 얻기
@@ -257,10 +258,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         builder.setNegativeButton("Close") { _, _ ->
             Thread {
-                val jsonDataComparison = sendJsonDataToServer(breakerId, lat, lng, "1")
+                val jsonDataComparison = sendJsonDataToServer(breakerId, lat, lng, "2")
 
                 if (jsonDataComparison == true) {
-                    sendJsonDataToServer(breakerId, lat, lng, "2")
                     runOnUiThread {
                         if (currentIcon != closeIcon) {
                             // 클릭된 마커의 아이콘을 변경
@@ -337,7 +337,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         //새로운 token이 생성될때마다 호출되는 callback
-        Log.d("onNewToken","${token}")
+//        Log.d("onNewToken","${token}")
+        Log.d(TAG,"Refresh token: $token")
         super.onNewToken(token)
     }
 
@@ -349,8 +350,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // Log.d(TAG, "Notification Message Body: " + remoteMessage.notification?.body!!)
 
         if(remoteMessage.data.isNotEmpty()){
-            Log.i("바디: ", remoteMessage.data["body"].toString())
             Log.i("타이틀: ", remoteMessage.data["title"].toString())
+            Log.i("바디: ", remoteMessage.data["body"].toString())
             sendNotification(remoteMessage)
         }
 
@@ -361,13 +362,13 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     }
 
     private fun sendNotification(remoteMessage: RemoteMessage) {
-        // RequestCode, Id를 고유값으로 지정하여 알림이 개별 표시되도록 함
+        // RequestCode, Id를 고유값으로 지정하여 알림이 개별 표시
         val uniId: Int = (System.currentTimeMillis() / 7).toInt()
 
         // 일회용 PendingIntent
-        // PendingIntent : Intent 의 실행 권한을 외부의 어플리케이션에게 위임한다.
+        // PendingIntent : Intent 의 실행 권한을 외부의 어플리케이션에게 위임
         val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) // Activity Stack 을 경로만 남긴다. A-B-C-D-B => A-B
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) // Activity Stack 을 경로만 남김 A-B-C-D-B => A-B
         val pendingIntent = PendingIntent.getActivity(this, uniId, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
         // 알림 채널 이름
@@ -377,19 +378,18 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // 알림 소리
         val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
-        // 알림에 대한 UI 정보와 작업을 지정한다.
+        // 알림에 대한 UI 정보와 작업을 지정
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.mipmap.ic_launcher) // 아이콘 설정
-            .setContentTitle(remoteMessage.data["body"].toString()) // 제목
-            .setContentText(remoteMessage.data["title"].toString()) // 메시지 내용
+            .setContentTitle(remoteMessage.data["title"].toString()) // 제목
+            .setContentText(remoteMessage.data["body"].toString()) // 메시지 내용
             .setAutoCancel(true)
             .setSound(soundUri) // 알림 소리
             .setContentIntent(pendingIntent) // 알림 실행 시 Intent
 
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        // 오레오 버전 이후에는 채널이 필요하다.
+        // 오레오 버전 이후는 채널 필요
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(channelId, "Notice", NotificationManager.IMPORTANCE_DEFAULT)
             notificationManager.createNotificationChannel(channel)
